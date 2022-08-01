80e00a45766969a02dac5b79d6df20635a141e25
Alexander <alex.krysiuk@gmail.com>
2022-08-01 23:45:12 +0200

    adding Battleship class

---
+++ Classes/Battleship.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Battleship
+{
+	string Input;		// field --> should specified
+	string Display;		// field --> should specified
+	while (true)		// loop
+	{
+		// code
+	}
+
+}
