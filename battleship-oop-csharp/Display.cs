17e0385f4b8f287001ba43b44a4f46951869e43c
abenteuerzeit <amroz.muzyka@gmail.com>
2022-08-03 12:19:48 +0200

    Create Display Class

---
+++ battleship-oop-csharp/Display.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace battleship_oop_csharp
+{
+    internal class Display
+    {
+        /*
+         * Implement the Display class and its methods.
+         * The Display class prints the game menu.
+         * The Display class prints the board during manual ship placement process.
+         * The Display class prints the gameplay.
+         * The Display class prints the outcome of the game when it is over.
+         * No Console.WriteLine() happens outside of the Display class. 
+         */
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+}
