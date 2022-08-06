470df7f55bdd16089865fad452b94760300827d2
Alexander <alex.krysiuk@gmail.com>
2022-08-06 19:57:07 +0200

    menu creation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("██████╗░░█████╗░████████╗████████╗██╗░░░░░███████╗░██████╗██╗░░██╗██╗██████╗░");
+Console.WriteLine("██╔══██╗██╔══██╗╚══██╔══╝╚══██╔══╝██║░░░░░██╔════╝██╔════╝██║░░██║██║██╔══██╗");
+Console.WriteLine("██████╦╝███████║░░░██║░░░░░░██║░░░██║░░░░░█████╗░░╚█████╗░███████║██║██████╔╝");
+Console.WriteLine("██╔══██╗██╔══██║░░░██║░░░░░░██║░░░██║░░░░░██╔══╝░░░╚═══██╗██╔══██║██║██╔═══╝░");
+Console.WriteLine("██████╦╝██║░░██║░░░██║░░░░░░██║░░░███████╗███████╗██████╔╝██║░░██║██║██║░░░░░");
+Console.WriteLine("╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░░░░╚═╝░░░╚══════╝╚══════╝╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░");
+Console.WriteLine("Game made by Adrian, Alex & Kasia in C#");
+Console.WriteLine("Enter \"1\": Start New Game");
+Console.WriteLine("Enter \"2\": View High Score");
+Console.WriteLine("Enter \"3\": Exit");
+int menuAction = Convert.ToInt32(Console.ReadLine());
+switch(menuAction)
+{
+    case 1:
+        Console.WriteLine("You started new game");
+        break;
+    case 2:
+        Console.WriteLine("You viewed highscore");
+        break;
+    case 3:
+        Console.WriteLine("You exit");
+        break;
+    default:
+        Console.WriteLine("Please choose right number");
+        break;
+}
